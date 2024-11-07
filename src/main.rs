6a99107b75a26a6c93f7bb1800deda43d9be2748
CompewterTutor <mike@3phase.com>
2024-11-07 14:19:39 -0700

    added libraries

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
 }
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    let main_window = MainWindow::new()?;
+
+    main_window.run()
+}
